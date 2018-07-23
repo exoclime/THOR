f7e755798d44c951e2a9f48c4b0467886ced3d02
Russell Deitrick <russell.deitrick@csh.unibe.ch>
2018-07-23 14:14:32 +0200

    fixed comment syntax so the compiler will stfu

--- src/headers/phy/profx_deepHJ_hs.h
+++ src/headers/phy/profx_deepHJ_hs.h
@@ -113,8 +113,8 @@ __global__ void deepHJ_hs(double *Mh_d         ,
                     + 0.16475329*Ptil*Ptil*Ptil + 0.014241552*Ptil*Ptil*Ptil*Ptil;
           kt_hs = pow(10,-logtrad); //      Temperature forcing constant.
         } else {
-          //Tnight = 5529.7168 - 6869.6504*Ptil + 4142.7231*Ptil*Ptil \
-                   - 936.23053*Ptil*Ptil*Ptil + 87.120975*Ptil*Ptil*Ptil*Ptil;
+          /*Tnight = 5529.7168 - 6869.6504*Ptil + 4142.7231*Ptil*Ptil \
+                   - 936.23053*Ptil*Ptil*Ptil + 87.120975*Ptil*Ptil*Ptil*Ptil;*/
           //The above seems to be a very poor fit to Tiro... switch to eq B4 instead
           Tnight = 1696.6986 + 132.2318*Ptil - 174.30459*Ptil*Ptil \
                    + 12.579612*Ptil*Ptil*Ptil + 59.513639*Ptil*Ptil*Ptil*Ptil \
