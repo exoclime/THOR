db1093e486c7d935c5681ea594409d8240f73008
Russell Deitrick <russell.deitrick@csh.unibe.ch>
2018-10-10 16:57:19 +0200

    started adding insolation calculation to profx_RT (not working yet)

--- src/physics/modules/inc/profx_RT.h
+++ src/physics/modules/inc/profx_RT.h
@@ -24,7 +24,104 @@
 //
 ////////////////////////////////////////////////////////////////////////
 
-__device__ void radcsw(double *phtemp         ,
+
+__host__ double solve_kepler(double mean_anomaly,
+                             double ecc         ) {
+
+// Solve Kepler's equation (see Murray & Dermott 1999)
+// Get eccentric anomaly from mean anomaly and eccentricity
+
+  double ecc_anomaly, fi, fi_1, fi_2, fi_3, di_1, di_2, di_3;
+
+  ecc_anomaly = mean_anomaly + sign(sin(mean_anomaly))*0.85*e;
+  di_3 = 1.0;
+
+  while (di_3 > 1e-15) {
+    fi = ecc_anomaly - ecc*sin(ecc_anomaly) - mean_anomaly;
+    fi_1 = 1.0 - ecc*cos(ecc_anomaly);
+    fi_2 = ecc*sin(ecc_anomaly);
+    fi_3 = ecc*cos(ecc_anomaly);
+    di_1 = -fi / fi_1;
+    di_2 = -fi / (fi_1 + 0.5*di_1*fi_2);
+    di_3 = -fi / (fi_1 + 0.5*di_2*fi_2 + 1./6.*di_2*di_2*fi_3);
+    ecc_anomaly = ecc_anomaly + di_3;
+  }
+  return ecc_anomaly;
+}
+
+__host__ double calc_r_orb(double ecc_anomaly,
+                           double ecc        ) {
+
+// Calc relative distance between planet and star (units of semi-major axis)
+
+  double r = 1.0 - ecc*cos(ecc_anomaly);
+  return r;
+}
+
+__host__ double ecc2true_anomaly(double ecc_anomaly,
+                                 double ecc        ){
+
+// Convert from eccentric to true anomaly
+
+  double tanf2, true_anomaly;
+  tanf2 = sqrt((1.0+ecc)/(1.0-ecc))*tan(ecc_anomaly/2.0);
+  true_anomaly = 2.0*atan(tanf2);
+  if (true_anomaly<0.0) true_anomaly += 2*M_PI;
+  return true_anomaly;
+}
+
+__host__ double true2ecc_anomaly(double true_anomaly,
+                                 double ecc         ){
+
+// Convert from true to eccentric anomaly
+
+  double cosE, ecc_anomaly;
+  while (true_anomaly<0.0) true_anomaly += 2*M_PI;
+  while (true_anomaly>=2*M_PI) true_anomaly -= 2*M_PI;
+  cosE = (cos(true_anomaly) + ecc) / (1.0 + ecc*cos(true_anomaly));
+  if (true_anomaly < M_PI){
+    ecc_anomaly = acos(cosE);
+  } else {
+    ecc_anomaly = 2*M_PI - acos(cosE);
+  }
+  return ecc_anomaly;
+}
+
+// need to calc mean_anomaly_i at start
+// ecc, obliquity, mean_motion, longp, true_long_i and alpha_i are user inputs
+__host__ void update_spin_orbit() {
+
+// Update the insolation related parameters for spin and orbit
+
+  mean_anomaly = fmod((mean_motion*time + mean_anomaly_i),(2*M_PI));
+
+  ecc_anomaly = fmod(solve_kepler(mean_anomaly, ecc),(2*M_PI));
+
+  r_orb = calc_r_orb(ecc_anomaly, ecc);
+  true_long = fmod((ecc2true_anomaly(ecc_anomaly, ecc) + longp),(2*M_PI));
+
+  sin_decl = sin(obliquity)*sin(true_long);
+  cos_decl = sqrt(1.0 - sin_decl*sin_decl);
+  alpha = -Omega*time + true_long - true_long_i + alpha_i;
+}
+
+__device__ double calc_insol(double *lonlat_d,  //latitude/longitude grid
+                             double alpha    ,  //current RA of star (relative to zero long on planet)
+                             double sin_decl ,  //declination of star
+                             double cos_decl ,
+                             double r_orb    ){ //host star planet distance (units of semi-major axis)
+
+// Calculate the insolation (scaling) at a point on the surface
+
+  double insol_q;
+
+  insol_q = pow(r,-2)*( sin(lonlat_d[id*nv+1])*sin_decl + \
+                      cos(lonlat_d[id*nv+1])*cos_decl*cos(lonlat_d[id*nv]-alpha) );
+  return insol_q;     //insolation scaling factor
+}
+
+
+__device__ void radcsw(double *phtemp     ,
                        double  coszrs     ,
                        double *dtemp      ,
                        double *tau_d      ,
