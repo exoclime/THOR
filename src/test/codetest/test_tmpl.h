8937b1dadc620027d5bdf4f9738204814b420f1d
nab <nab@lampshade.ch>
2018-10-19 09:21:34 +0200

    fix compile warnings on esp_initial.cu fscanf function, add warnings to Makefile, add some code experimenting for templates

---
+++ src/test/codetest/test_tmpl.h
@@ -0,0 +1,41 @@
+// Simple test for template specialisation, with just one specialised function
+// build with  g++ test_tmpl.cpp -o test_tmpl 
+
+#include <iostream>
+
+
+class test_interface
+{
+public:
+    virtual void print2() = 0;
+};
+
+
+template<typename T>
+class test : public test_interface
+{
+public:
+    void print(T in)
+    {
+        std::cout << "Test: "<< in << std::endl;
+    };
+
+    void print2()
+    {
+        std::cout << "Test2 " << std::endl;
+    };
+
+        
+};
+
+template<>
+void test<bool> ::print2()
+    {
+        std::cout << "Test2 specialised " << std::endl;
+    };
+
+
+    
+
+
+    
