e87f5e19b0480d1d05ca98728545706121bed317
Urs Schroffenegger <nab@lampshade.ch>
2018-11-27 13:39:23 +0100

    add some variables to output file, fixes issue #25

--- src/output/esp_output.cu
+++ src/output/esp_output.cu
@@ -186,6 +186,28 @@ __host__ void ESP::output(int                    fidx, // Index of output file
         //      DivDampP option
         s.append_value(sim.DivDampP ? 1.0 : 0.0, "/DivDampP", "-", "Using Divergence-damping");
 
+        //      HyDiff option
+        s.append_value(sim.HyDiff ? 1.0 : 0.0, "/HyDiff", "-", "Using Hyper diffusion");
+
+        //      Hyperdiffusion strength 
+        s.append_value(sim.Diffc, "/Diffc", "-", "Hyperdiffusion strength");
+
+        //      Tmean
+        s.append_value(sim.Tmean, "/Tmean", "K", "Mean atmospheric temperature");
+
+        //      conv adj option
+        s.append_value(sim.conv_adj, "/conv_adj", "-", "Using convection adjustment");
+
+        //      GCM on/off option
+        s.append_value(sim.gcm_off ? 1.0 : 0.0, "/gcm_off", "-", "GCM off");
+
+        //      rest option
+        s.append_value(sim.rest ? 1.0 : 0.0, "/rest", "-", "Starting from rest");
+
+        //      TPprof option
+        s.append_value(sim.TPprof, "/TPprof", "-", "Initial TP profile option");
+
+
         //      core_benchmark  option
         s.append_value(int(core_benchmark), "/core_benchmark", "-", "Using benchmark forcing or RT");
         if (sim.SpongeLayer) {
@@ -196,8 +218,6 @@ __host__ void ESP::output(int                    fidx, // Index of output file
             //      Rv
             s.append_value(Rv_sponge, "/Rv_sponge", "1/s", "Strength of sponge layer");
         }
-
-        s.append_value(
 
         // store module name in the description
         s.append_value(0.0, "/phy_module", "-", phy_modules_get_name());
