814dd4fca4fbafecda7098c219176fb1e307b023
Russell Deitrick <russell.deitrick@csh.unibe.ch>
2019-07-08 11:21:05 +0200

    moved RT heating into core

--- src/headers/dyn/thor_fastmodes.h
+++ src/headers/dyn/thor_fastmodes.h
@@ -629,8 +629,8 @@ __global__ void Density_Pressure_Eqs_Poles(double *pressure_d,
             // Updates pressure
             // p                         = P_Ref * pow(Rd * aux / P_Ref, Cp / Cv);
             pressure_d[id * nv + lev] = p - pressurek_d[id * nv + lev]
-                                        + (diffpr_d[id * nv + lev] + diffprv_d[id * nv + lev]) * dt;
-            // + profx_dP_d[id * nv + lev] * dt;
+                                        + (diffpr_d[id * nv + lev] + diffprv_d[id * nv + lev]) * dt
+                                        + profx_dP_d[id * nv + lev] * dt;
             // if (isnan(pressure_d[id * nv + lev])) {
             //     printf("(id, lev) = (%d, %d)", id, lev);
             // }
