ed2896d5bbd5299441a7420ef97e57bb4e67c980
Urs Schroffenegger <urss@hulk.unibe.ch>
2020-03-10 13:05:28 +0100

    batch for alf test

--- tools/slurm_batch_run.py
+++ tools/slurm_batch_run.py
@@ -6,16 +6,16 @@ import subprocess
 import re
 
 working_dir = "/home/urss/prog/THOR-dev/"
-job_name = "earthalfpr"
-initial_file = "ifile/earth_alf.thr"
+job_name = "wasp_alf_alf"
+initial_file = "ifile/alf_wasp_alf.thr"
 # Acceptable  time  formats  include  "minutes", "minutes:seconds",
 # "hours:minutes:seconds", "days-hours",
 # "days-hours:minutes" and "days-hours:minutes:seconds".
-time_limit = "2-0:0"
+time_limit = "0-12:0"
 mail = "urs.schroffenegger@csh.unibe.ch"
 
-# profiling=None
-profiling = f"{job_name}.prof"
+profiling=None
+#profiling = f"{job_name}.prof"
 
 
 output_file = f"/home/urss/slurm-esp-{job_name}-%j.out"
@@ -30,8 +30,9 @@ args = ['sbatch',
         '--mail-user='+mail,
         '--output='+output_file,
         '--signal=INT@60']
-esp_command = "bin/esp"
-esp_args = f'-b -N 20 -o {job_name}'
+esp_command = "bin/esp_alf"
+#esp_args = f'-b -N 20 -o {job_name}'
+esp_args = f'-b -o {job_name}'
 num_jobs = 1
 
 def start_esp(args, esp_command, esp_args, initial_file, profiling=None):
