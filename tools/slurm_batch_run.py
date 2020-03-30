8de8a8c4b5711ecf1451f0aecba42657f407aa26
Urs Schroffenegger <nab@lampshade.ch>
2020-03-30 11:54:07 +0200

    slurm batch tool, alf

--- tools/slurm_batch_run.py
+++ tools/slurm_batch_run.py
@@ -5,6 +5,35 @@ Queue multiple tasks for slurm
 import subprocess
 import re
 
+
+def start_esp(args, esp_command, esp_args, initial_file, profiling=None):
+    batch_id_re = re.compile("Submitted batch job (\d+)\n")
+
+    with subprocess.Popen(args,
+                          stdin=subprocess.PIPE,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE,
+                          universal_newlines=True) as proc:
+        if profiling is not None:
+            prof_cmd = f"nvprof --export-profile {profiling}"
+        else:
+            prof_cmd = ''
+        proc.stdin.write("#!/bin/sh\n")
+        proc.stdin.write(f"srun {prof_cmd} {esp_command} {esp_args} {initial_file}\n")
+        proc.stdin.close()
+        procid_string = proc.stdout.read()
+        # print(procid_string)
+        m = batch_id_re.match(procid_string)
+        if m is not None:
+            batch_id = m.group(1)
+            print("Submitted batch with ID: {}".format(batch_id))
+            return True, batch_id
+        else:
+            print("Error reading batch id from return: {}".format(procid_string))
+
+            return False, -1
+
+
 working_dir = "/home/urss/prog/THOR-dev/"
 job_name = "wasp_alf_alf"
 initial_file = "ifile/alf_wasp_alf.thr"
@@ -14,7 +43,7 @@ initial_file = "ifile/alf_wasp_alf.thr"
 time_limit = "0-12:0"
 mail = "urs.schroffenegger@csh.unibe.ch"
 
-profiling=None
+profiling = None
 #profiling = f"{job_name}.prof"
 
 
@@ -27,40 +56,14 @@ args = ['sbatch',
         '-p', 'production',
         '--time', time_limit,
         '--mail-type=ALL',
-        '--mail-user='+mail,
-        '--output='+output_file,
+        '--mail-user=' + mail,
+        '--output=' + output_file,
         '--signal=INT@60']
 esp_command = "bin/esp_alf"
 #esp_args = f'-b -N 20 -o {job_name}'
 esp_args = f'-b -o {job_name}'
 num_jobs = 1
 
-def start_esp(args, esp_command, esp_args, initial_file, profiling=None):
-    batch_id_re = re.compile("Submitted batch job (\d+)\n")
-
-    with subprocess.Popen(args,
-                          stdin=subprocess.PIPE,
-                          stdout=subprocess.PIPE,
-                          stderr=subprocess.PIPE, 
-                          universal_newlines=True) as proc:
-        if profiling is not None:
-            prof_cmd = f"nvprof --export-profile {profiling}"
-        else:
-            prof_cmd = ''
-        proc.stdin.write("#!/bin/sh\n")
-        proc.stdin.write(f"srun {prof_cmd} {esp_command} {esp_args} {initial_file}\n")
-        proc.stdin.close()
-        procid_string = proc.stdout.read()
-        #print(procid_string)
-        m = batch_id_re.match(procid_string) 
-        if m is not None:
-            batch_id = m.group(1)
-            print("Submitted batch with ID: {}".format(batch_id))
-            return True, batch_id
-        else:
-            print("Error reading batch id from return: {}".format(procid_string))
-            
-            return False, -1
 
 last_success = None
 last_id = None
