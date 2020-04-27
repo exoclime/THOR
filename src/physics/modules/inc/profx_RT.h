5b288c545fd236c74faa357e677f75c2c9a4459a
nab <nab@halo.lampshade.ch>
2020-04-27 14:26:57 +0200

    add scaling factor for qheat in radiative transfer for usage as Alf spinup

--- src/physics/modules/inc/profx_RT.h
+++ src/physics/modules/inc/profx_RT.h
@@ -413,6 +413,7 @@ __global__ void rtm_dual_band(double *pressure_d,
                               double *surf_flux_d,
                               double *profx_Qheat_d,
                               double *Rd_d,
+			      double  Qheat_scaling,
                               bool    gcm_off,
                               bool    rt1Dmode) {
 
@@ -585,7 +586,7 @@ __global__ void rtm_dual_band(double *pressure_d,
                     //trying to prevent too much cooling resulting in negative pressure in dyn core
                     dtemp[id * nv + lev] = -pressure_d[id * nv + lev] / timestep;
                 }
-                profx_Qheat_d[id * nv + lev] = dtemp[id * nv + lev];
+                profx_Qheat_d[id * nv + lev] = Qheat_scaling*dtemp[id * nv + lev];
             }
         }
     }
