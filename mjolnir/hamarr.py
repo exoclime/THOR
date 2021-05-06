092cc778d92a344ead227e0c82cf5f1e3c9a37c1
Russell Deitrick <russell.deitrick@csh.unibe.ch>
2021-05-06 16:17:02 +0200

    cleaned up some hacks, added a few things to mjolnir plotting

--- mjolnir/hamarr.py
+++ mjolnir/hamarr.py
@@ -2040,9 +2040,11 @@ def profile(input, grid, output, z, stride=50, axis=None, save=True, use_p=True,
                     path_effects=[pe.Stroke(linewidth=1.5, foreground=color), pe.Normal()])
 
         # if use_p:
-        #     ax.plot(z['value'][2264 ,:,0],output.Pressure[2264,:,0]/1e5,'r--',zorder=111)
-        #     # ax.plot(z['value'][1629,:,0],output.Pressure[1629,:,0]/1e5,'r--',zorder=111)
-        #
+        #     for jj in [2561]:
+        #         ax.plot(z['value'][jj ,:,0],output.Pressure[jj,:,0]/1e5,'r--',zorder=111)
+        #         col_lon.append(grid.lon[jj])
+        #         col_lat.append(grid.lat[jj])
+        #         col_lor.append('r')
         # else:
         #     ax.plot(z['value'][1995,:,0],grid.Altitude,'r--',zorder=111)
         rp, = ax.plot(x[np.int(np.floor(grid.nv / 2))], y[np.int(np.floor(grid.nv / 2))] * unit, 'k+', ms=5, alpha=0.5)
