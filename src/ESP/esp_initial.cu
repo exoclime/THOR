cf64195e986bce533f58b44eebef795173e05c40
P. Noti <pascal_noti@hotmail.com>
2021-07-26 11:20:07 +0200

    errors

--- src/ESP/esp_initial.cu
+++ src/ESP/esp_initial.cu
@@ -476,6 +476,8 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
             printf(" before any condition for init_PT_profile ");
 
             printf("init_PT_profile: %d\n", init_PT_profile);
+            printf("PARMENTIER: %d\n", PARMENTIER);
+
             if ((init_PT_profile == ISOTHERMAL || init_PT_profile == CONSTBV)
                 && ultrahot_thermo == NO_UH_THERMO) {
                 //isothermal initial profile, no variation in Rd or Cp due to H-H2 reaction
