2058aca9fa758d4b17fb024990aa8cc6264aef4d
P. Noti <pascal_noti@hotmail.com>
2021-07-29 15:16:10 +0200

    errors

--- src/ESP/esp_initial.cu
+++ src/ESP/esp_initial.cu
@@ -662,7 +662,7 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
                 const double StBC = 5.670374419e-8;
                 const double pi = atan((double)(1)) * 4;
 
-                double temperatureh_h[nvi]};
+                double temperatureh_h[nvi];
                 double pressureh_d[nvi];
 
                
