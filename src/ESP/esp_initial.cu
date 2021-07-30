9f043891d274d8425df19e750a015a7f2b8435b1
P. Noti <pascal_noti@hotmail.com>
2021-07-30 16:37:34 +0200

    ee

--- src/ESP/esp_initial.cu
+++ src/ESP/esp_initial.cu
@@ -825,7 +825,7 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
                 Parmentier_IC(i, nv, pressure_h, Tint, mu, Tirr, sim.Gravit, temperature_h, table_num, met);
                 adiabat_correction(i, nv, temperature_h, pressure_h, sim.Gravit);
 
-                double temp_temp[nv];
+                double temp_temp[nv*2];
 
                 for (int lev = 0; lev < nv; lev++) {                
                     
