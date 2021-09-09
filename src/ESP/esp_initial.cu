4f1344bf70b04a9533cffa93feeacb141fa4e38d
P. Noti <pascal_noti@hotmail.com>
2021-09-09 14:50:36 +0200

    ee

--- src/ESP/esp_initial.cu
+++ src/ESP/esp_initial.cu
@@ -835,7 +835,12 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
                 }
                 
                 printf(" before Tirr and Parmentier \n");
-                Tirr = Tstar * pow(radius_star / planet_star_dist ,0.5);
+                Tirr = Tstar * pow(radius_star / (planet_star_dist*1.496e+11) ,0.5);
+
+                printf("Tstar %e  \n", Tstar);
+                printf("radius_star %e  \n", radius_star);
+                printf("planet_star_dist %e  \n", planet_star_dist);
+                printf("Tirr %e  \n", Tirr);
 
                 double OpaTableTemperature__h[1060];
                 text_file_to_array("src/physics/modules/src/OpaTableTemperature.txt" , OpaTableTemperature__h, 1060);
