b96b19731a0a869729194a187a62ef0283ad1268
P. Noti <pascal_noti@hotmail.com>
2021-09-16 15:44:25 +0200

    e

--- src/headers/phy/init_PT_profile.h
+++ src/headers/phy/init_PT_profile.h
@@ -1150,7 +1150,7 @@ void bottum_up_adiabat_correction(int id, int nlay, double* (&Tl), double* press
     // work variables
     double lapse_rate;
     double T_rate;
-    double lapse_rate_factor = 1.0;
+    double lapse_rate_factor = 1.2;
 
     // start operations
 
