b8763929738d8d7b72f655a52cc65169ae545f18
P. Noti <pascal_noti@hotmail.com>
2021-09-18 14:03:37 +0200

    e

--- src/ESP/esp_initial.cu
+++ src/ESP/esp_initial.cu
@@ -782,13 +782,14 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
                         printf("Rho_h[i * nv + %d] = %e \n", lev, Rho_h[i * nv + lev]);
                         printf("temperature_h[i * nv + %d] = %e \n", lev, temperature_h[i * nv + lev]);
                     }
-                    if (iter < max_iter-1) {
+                    if (iter < max_iter-2) {
                         Parmentier_bilinear_interpolation_IC(i, nv, pressure_h, Tint, mu, Tirr,
                             OpaTableTemperature__h, OpaTablePressure__h, OpaTableKappa__h, sim.Gravit, temperature_h, table_num, MetStar);
+                    }
+                    if (iter < max_iter-1) {
                         adiabat_correction(i, nv, temperature_h, pressure_h, sim.Gravit);        
                         bottum_up_adiabat_correction(i, nv, temperature_h, pressure_h, sim.Gravit, Cp_h, Altitude_h, Cp_h, Rd_h,sim.P_Ref);
                     }
-                   
                     
                     printf("pressure_h[i * nv + nv - 1] = %e \n", pressure_h[i * nv + nv - 1]);
                     printf("Rho_h[i * nv + nv - 1] = %e \n", Rho_h[i * nv + nv - 1]);
