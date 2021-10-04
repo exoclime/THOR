d315f343705e2b960822ab0f1a02b5bb552ebd04
P. Noti <pascal_noti@hotmail.com>
2021-10-04 12:47:25 +0200

    e

--- src/headers/phy/dry_conv_adj.h
+++ src/headers/phy/dry_conv_adj.h
@@ -85,7 +85,7 @@ __global__ void dry_conv_adj(double timestep,       // time step [s]
         int  iter   = 0;
         bool repeat = true; //will repeat entire
 
-        bool ray_mode = true;
+        bool ray_mode = false;
 
         if (ray_mode == true)
         {
