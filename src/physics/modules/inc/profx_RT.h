56c6afa16baea84c9275c10360174a970e83b595
P. Noti <pascal_noti@hotmail.com>
2021-10-07 10:33:35 +0200

    e

--- src/physics/modules/inc/profx_RT.h
+++ src/physics/modules/inc/profx_RT.h
@@ -1296,7 +1296,7 @@ __device__  void lw_grey_updown_linear(int id,
             }
         }
 
-        if (mu_s[id]<0.001 && mu_s[id]>0.0)
+        if (mu_s[id]<0.6 && mu_s[id]>0.4)
         {
             for (int i = 30; i < nlay1; i++)
             {
@@ -1633,7 +1633,7 @@ __global__ void rtm_picket_fence(double *pressure_d,
 
         // !! Radiation - Comment in what scheme you want to use - Heng model won't work!
         
-        if (zenith_angles[id] > 0.9 && zenith_angles[id] < 1.0) {
+        if (zenith_angles[id] > 0.4 && zenith_angles[id] < 0.6) {
             
             flux_top = (1.0 - AB_d[id]) *  F0_d ; // * (1-alb);
             insol_d[id] = flux_top;
