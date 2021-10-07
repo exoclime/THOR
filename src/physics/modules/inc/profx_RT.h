d3a1c66b5a0c6f1cb3be91a044a7974bb35d64f8
P. Noti <pascal_noti@hotmail.com>
2021-10-07 12:03:23 +0200

    ee

--- src/physics/modules/inc/profx_RT.h
+++ src/physics/modules/inc/profx_RT.h
@@ -1640,19 +1640,19 @@ __global__ void rtm_picket_fence(double *pressure_d,
         
         //if (zenith_angles[id] > 0.4 && zenith_angles[id] < 0.6) {
         if (id==340) {
-            printf("Teff[%d] = %e \n", 0, Teff[0]);
-            printf("AB__h[%d] = %e \n", 0, AB__h[0]);
-            printf("gam_V__h[0 + 0] = %e \n", gam_V__h[0]);
-            printf("gam_V__h[0 + 1] = %e \n", gam_V__h[1]);
-            printf("gam_V__h[0 + 2] = %e \n", gam_V__h[2]);
-            printf("Beta_V__h[0 + 0] = %e \n", Beta_V__h[0]);
-            printf("Beta_V__h[0 + 1] = %e \n", Beta_V__h[1]);
-            printf("Beta_V__h[0 + 2] = %e \n", Beta_V__h[2]);
-            printf("Beta__h[0 + 0] = %e \n", Beta__h[0]);
-            printf("Beta__h[0 + 1] = %e \n", Beta__h[1]);
-            printf("gam_1__h[0] = %e \n", gam_1__h[0]);
-            printf("gam_2__h[0] = %e \n", gam_2__h[0]);
-            printf("gam_P[0] = %e \n", gam_P[0]);
+            //printf("Teff[%d] = %e \n", 0, Teff[0]);
+            printf("AB_d[%d] = %e \n", 0, AB__h[0]);
+            printf("gam_V_3_d[0 + 0] = %e \n", gam_V__h[0]);
+            printf("gam_V_3_d[0 + 1] = %e \n", gam_V__h[1]);
+            printf("gam_V_3_d[0 + 2] = %e \n", gam_V__h[2]);
+            printf("Beta_V_3_d[0 + 0] = %e \n", Beta_V__h[0]);
+            printf("Beta_V_3_d[0 + 1] = %e \n", Beta_V__h[1]);
+            printf("Beta_V_3_d[0 + 2] = %e \n", Beta_V__h[2]);
+            printf("Beta_2_d[0 + 0] = %e \n", Beta__h[0]);
+            printf("Beta_2_d[0 + 1] = %e \n", Beta__h[1]);
+            printf("gam_2_d[0] = %e \n", gam_1__h[0]);
+            printf("gam_1_d[0] = %e \n", gam_2__h[0]);
+            //printf("gam_P[0] = %e \n", gam_P[0]);
             printf("mu[0] = %e \n", 0.99);
             for (int level = 0; level < nv; level++)
             {
