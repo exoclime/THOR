c6712643c76c20f9d1d3ff505b0993ec976e4642
P. Noti <pascal_noti@hotmail.com>
2021-10-09 12:50:23 +0200

    e

--- src/headers/phy/dry_conv_adj.h
+++ src/headers/phy/dry_conv_adj.h
@@ -528,7 +528,7 @@ __global__ void ray_dry_conv_adj(double timestep,       // time step [s]
 
             // constants & parameters
 
-            int itermax1 = 1;
+            int itermax1 = 2;
             int itermax2 = 5;
             const double small = 1e-6;
             double const euler = 2.71828182845904523536028;
@@ -640,7 +640,8 @@ __global__ void ray_dry_conv_adj(double timestep,       // time step [s]
                         asm("trap;");            // kill kernel with error
                     }
                     
-                    Temperature_d[id * nv + i] = Temperature_d[id * nv + i] + (dT_conv_d[id * nv + i] - Temperature_d[id * nv + i])  / timestep;
+                    Temperature_d[id * nv + i] = Temperature_d[id * nv + i] + (dT_conv_d[id * nv + i] - Temperature_d[id * nv + i])  /
+                        (itermax1 *timestep);
                     
                 }
                 for (i = 1; i <nv; i++){
@@ -656,7 +657,7 @@ __global__ void ray_dry_conv_adj(double timestep,       // time step [s]
                                     0.5*(Rd_d[id * nv + i] + Rd_d[id * nv + i -1])
                                 )
                             ) -
-                        Pressure_d[id * nv + i]) / timestep ;
+                        Pressure_d[id * nv + i]) / (timestep * itermax1) ;
                     
                     if (id==0 && isnan(Pressure_d[id * nv + i]))
                     {
