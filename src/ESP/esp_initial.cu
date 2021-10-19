f50df784fec85efcff0cfb6101ca598db7ac8ad6
P. Noti <pascal_noti@hotmail.com>
2021-10-19 12:51:09 +0200

    e

--- src/ESP/esp_initial.cu
+++ src/ESP/esp_initial.cu
@@ -647,14 +647,7 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
                     printf(" init_temperature_parmentier[%d] = %e \n", lev,  init_temperature_parmentier[lev]);
                 }   
                 
-                printf(" Tirr = %e \n", Tirr);
-                printf(" radius_star = %e \n", radius_star);
-                printf(" planet_star_dist = %e \n", planet_star_dist);
-                printf(" Tstar = %e \n", Tstar);
-                printf(" init_temperature_parmentier[%d] = %e \n", 0,  init_temperature_parmentier[0]);
-                printf(" init_temperature_parmentier[%d] = %e \n", nv-1,  init_temperature_parmentier[nv-1]);
-                printf(" init_pressure_parmentier[%d] = %e \n", 0,  init_pressure_parmentier[0]);
-                printf(" init_pressure_parmentier[%d] = %e \n", nv-1,  init_pressure_parmentier[nv-1]);
+                
 
 
                 //Hypsometric equation
@@ -668,6 +661,17 @@ __host__ bool ESP::initial_values(const std::string &initial_conditions_filename
                         init_altitude_parmentier[level-1];
 
                 }
+
+                printf(" Tirr = %e \n", Tirr);
+                printf(" radius_star = %e \n", radius_star);
+                printf(" planet_star_dist = %e \n", planet_star_dist);
+                printf(" Tstar = %e \n", Tstar);
+                printf(" init_temperature_parmentier[%d] = %e \n", 0,  init_temperature_parmentier[0]);
+                printf(" init_temperature_parmentier[%d] = %e \n", nv-1,  init_temperature_parmentier[nv-1]);
+                printf(" init_pressure_parmentier[%d] = %e \n", 0,  init_pressure_parmentier[0]);
+                printf(" init_pressure_parmentier[%d] = %e \n", nv-1,  init_pressure_parmentier[nv-1]);
+                printf(" init_altitude_parmentier[%d] = %e \n", 0,  init_altitude_parmentier[0]);
+                printf(" init_altitude_parmentier[%d] = %e \n", nv-1,  init_altitude_parmentier[nv-1]);
 
 
                 for (int level = 0; level < nv; level++) {
