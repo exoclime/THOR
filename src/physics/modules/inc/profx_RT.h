4a84627511ab40f55c292e4fea3dbe08f467ef1a
P. Noti <pascal_noti@hotmail.com>
2021-10-26 22:20:47 +0200

    e

--- src/physics/modules/inc/profx_RT.h
+++ src/physics/modules/inc/profx_RT.h
@@ -956,7 +956,7 @@ __device__ void tau_struct(int id,
            tau_lay  =   (   0.25 * kRoss[id*nlay*nchan + channel * nlay + level + 1] +
                             0.5  * kRoss[id*nlay*nchan + channel * nlay + level] +
                             0.25 * kRoss[id*nlay*nchan + channel * nlay + level - 1]
-                        ) * delPdelAlt *;
+                        ) * delPdelAlt *
                         (   0.25 *  Rho_d[id*nlay  + level + 1] +
                             0.5  *  Rho_d[id*nlay  + level] +
                             0.25 *  Rho_d[id*nlay  + level - 1]
